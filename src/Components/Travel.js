eca1fff72f852a2dcecb42af3370d115c39c16c7
LF <adam-c1991@hotmail.com>
2022-11-16 22:50:13 +0000

    add travel map

--- src/Components/Travel.js
+++ src/Components/Travel.js
@@ -6,6 +6,12 @@ export const Travel = () => {
       <section>
         <h1>TRAVEL</h1>
         <p>TRAVEL</p>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19685.804917315774!2d-0.9898341801784837!3d51.92071701904166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876e4a46413ab33%3A0x60fad75e2635f819!2sWinters%20Tale%20Country%20Barn!5e0!3m2!1sen!2suk!4v1668636950798!5m2!1sen!2suk"
+          width="600"
+          height="450"
+          loading="lazy"
+        ></iframe>
       </section>
     </div>
   );
