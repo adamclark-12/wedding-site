bb8b3ad8d834bedc1c105105ba4b9c6bf52e6c9e
LF <adam-c1991@hotmail.com>
2022-12-28 14:34:49 +0000

    mobile viewport break changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,16 +5,16 @@ export const Home = () => {
     <header className="title-contianer">
       <h1 className="title-h1">Fiona & Adam</h1>
       <h3 className="title-h3">11.11.23</h3>
+      <p>Hopefully below will be all the information you need.</p>
       <p>
-        Hopefully below will be all the information you need.
-        <br />
-        In case it isn't then dont be afraid to get <br /> in touch with us and
-        we will help all we can!
-        <br /> We hope you can make it and celebrate with us on our big day!
-        <br />
-        <br />
+        In case it isn't then dont be afraid to get in touch with us and we will
+        help all we can!
+      </p>
+      <p>We hope you can make it and celebrate with us on our big day!</p>
+      <p>
         <img src="weddingTimeline.jpg" alt="Wedding timeline"></img>
-        <br />
+      </p>
+      <p>
         <span className="bold-span">
           Get your beautiful selves there 20-30 minutes before kick off!
           <br />
